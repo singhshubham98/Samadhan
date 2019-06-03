c1e52eb8dc6554f713b135ec09e9df1be2b16046
Shubham Singh <201652026@iiitvadodara.ac.in>
2019-06-03 13:42:32 +0530

    Initial commit

--- chat/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-     url(r'^', views.home),
-    url(r'^(?P<room_id>[^/]+)', views.home),
-    url(r'^rooms/(?P<room_id>[^/]+)/messages/', views.messages),
-]
